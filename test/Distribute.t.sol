3c924920f19b16319681ac5caf96f85fa9dfb28b
eugenioclrc <eugenioclrc@gmail.com>
2022-10-22 00:20:17 -0300

    add buisiness logic test

--- test/Distribute.t.sol
+++ test/Distribute.t.sol
@@ -5,10 +5,10 @@ import "forge-std/Test.sol";
 import {Distribute} from "../src/Distribute.sol";
 
 contract DistributeTest is Test {
-    Distribute public distributeContract;
+    Distribute public instance;
 
     function setUp() public {
-        distributeContract = new Distribute{value: 1 ether}(
+        instance = new Distribute{value: 1 ether}(
             [makeAddr("user1"),
             makeAddr("user2"),
             makeAddr("user3"),
@@ -16,20 +16,32 @@ contract DistributeTest is Test {
         );
     }
 
-    function testPayable() public {        
+    function testPayable() public {
         vm.expectRevert();
-        address(distributeContract).call{value: 1}(abi.encodeWithSignature("distribute()"));
+        address(instance).call{value: 1}(abi.encodeWithSignature("distribute()"));
     }
 
-    function testGas() public {
+    function testGasTarget() public {
         uint256 TARGET_GAS_PRICE = 57_044;
         vm.warp(8 days);
 
         uint256 gasStart = gasleft();
-        distributeContract.distribute();
+        instance.distribute();
         uint256 totalGas = gasStart - gasleft();
         console.log("Total spend gas: ", totalGas);
         assertLt(totalGas, TARGET_GAS_PRICE, "Please save more gas");
     }
 
+    function testBusinessLogic() public {
+        vm.expectRevert("cannot distribute yet");
+        instance.distribute();
+
+        vm.warp(8 days);
+        instance.distribute();
+
+        assertEq(makeAddr("user1").balance, 0.25 ether);
+        assertEq(makeAddr("user2").balance, 0.25 ether);
+        assertEq(makeAddr("user3").balance, 0.25 ether);
+        assertEq(makeAddr("user4").balance, 0.25 ether);
+    }
 }
